ba60e5ffbbfc1a8fe173fd0936721a3ef9f4f60b
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2020-05-14 19:23:35 +0200

    Modification of PinsController. We add a constructor.

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -3,25 +3,31 @@
 namespace App\Controller;
 
 use App\Entity\Pin;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PinsController extends AbstractController
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+    
     /**
      * @Route("/pins", name="pins")
      */
     public function index()
     {
         $pin = new Pin;
-        $pin->setTitle('Title 1');
-        $pin->setDescription('Description 1');
+        $pin->setTitle('Title 2');
+        $pin->setDescription('Description 2');
 
-        $em = $this->getDoctrine()->getManager();
+        $this->em->persist($pin);
 
-        $em->persist($pin);
-
-        $em->flush();
+        $this->em->flush();
 
         return $this->render('pins/index.html.twig');
     }
